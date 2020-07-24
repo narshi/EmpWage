b954fab5f167aa97741ba77f2c70dcd18ad83468
Narayan <Shashankhegde32@gmaol.com>
2020-07-24 23:29:21 +0530

    Multiple companies

--- TerminalCommands/StageFour/Attendance.java
+++ TerminalCommands/StageFour/Attendance.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 public class Attendance{
 
+ private final String companyName;
  private final int empWagePerHrs;
  private final int daysPerMonth;
  private final int totalMaxHr;
@@ -12,24 +13,25 @@ public class Attendance{
  private final int fullTimeHrs;
 
   //Parameterized constructor
- public Attendance(int empWagePerHrs, int daysPerMonth, int totalMaxHr, int partTimeHrs, int fullTimeHrs)
+  public Attendance(int empWagePerHrs, int daysPerMonth, int totalMaxHr, int partTimeHrs, int fullTimeHrs, String companyName)
  {
-      this.empWagePerHrs=empWagePerHrs;
+   this.empWagePerHrs=empWagePerHrs;
    this.daysPerMonth=daysPerMonth;
    this.totalMaxHr=totalMaxHr;
    this.partTimeHrs=partTimeHrs;
    this.fullTimeHrs=fullTimeHrs;
+   this.companyName=companyName;
  }
 
  public int empAttendance()
  {
    Random myRan=new Random();
    int myRanVar=myRan.nextInt(3);
-   System.out.println("\nRandom Integers: "+myRanVar);
+   //System.out.println("\nRandom Integers: "+myRanVar);
    return myRanVar;
  }
 
- public void calSalary()
+ public int calSalary()
  {
     int empHrs=0,empSalary=0,TotalEmpSalary=0,totalHr=0,i=0;
 
@@ -39,36 +41,41 @@ public class Attendance{
       i++;
       switch(resultAttendance){
            case 0:
-                  System.out.println("Employee is Absent");
+                  //System.out.println("Employee is Absent");
                   empHrs=0;
                   break;
            case 1:
-                  System.out.println("Employee is Present: PART TIME");
+                  //System.out.println("Employee is Present: PART TIME");
                   empHrs=partTimeHrs;
                   break;
            case 2:
-                  System.out.println("Employee is Present: FULL TIME");
+                  //System.out.println("Employee is Present: FULL TIME");
                   empHrs=fullTimeHrs;
                   break;
           default : System.out.println("Invalid");
       }
      totalHr=totalHr+empHrs;
-     System.out.println("DAY"+i+" : Empolyee Hours: "+empHrs);
-     //empSalary=empWagePerHrs*totalHr;
-     //TotalEmpSalary=empSalary+TotalEmpSalary;
     }
     empSalary=empWagePerHrs*totalHr;
-    System.out.println("----------------------------------------------");
-    System.out.println("Total Hours: "+ totalHr);
-    System.out.println("Total Salary: "+ empSalary);
+    return empSalary;
  }
 
  public static void main (String args[]){
-     System.out.println("---------------------------------------------------");
-     System.out.println("WELCOME TO EMPLOYEE WAGE PROBLEM");
-     System.out.println("---------------------------------------------------");
-     Attendance ewc=new Attendance(20,20,100,4,8);
-     ewc.calSalary();
+     System.out.println("------------------------------------------------------------------------------");
+     System.out.println("                        WELCOME TO EMPLOYEE WAGE PROBLEM                      ");
+     System.out.println("------------------------------------------------------------------------------");
+
+     Attendance relianceCompany=new Attendance(20,20,100,4,8,"Reliance Smart");
+     int result1=relianceCompany.calSalary();
+     System.out.println("Company Name: "+relianceCompany.companyName);
+     System.out.println("The Employee Wage for company: "+result1);
+     System.out.println("------------------------------------------------------------------------------");
+
+     Attendance dmartCompany=new Attendance(22,22,100,5,9,"Dmart");
+     int result2=dmartCompany.calSalary();
+     System.out.println("Company Name: "+dmartCompany.companyName);
+     System.out.println("The Employee Wage for company: "+result2);
+     System.out.println("------------------------------------------------------------------------------");
  }
 
 }
