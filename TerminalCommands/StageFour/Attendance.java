0227e8e19972bd268f2c1c9f81cb0282244b60b7
Narayan <Shashankhegde32@gmaol.com>
2020-07-24 22:36:42 +0530

    Employee salary calculation

--- TerminalCommands/StageFour/Attendance.java
+++ TerminalCommands/StageFour/Attendance.java
@@ -5,31 +5,56 @@ import java.util.ArrayList;
 
 public class Attendance{
 
+  int empWagePerHrs,empHrs,empSalary;
+
  Attendance()
  {
    System.out.println("Default Constructor called");
  }
 
- public void empAttendance()
+  //Parameterized constructor
+ public Attendance(int wagePerHrs, int Hrs)
+ {
+      empWagePerHrs=wagePerHrs;
+      empHrs=Hrs;
+      empSalary=0;
+ }
+
+ public int empAttendance()
  {
    Random myRan=new Random();
    int myRanVar=myRan.nextInt(2);
    System.out.println("Random Integers: "+myRanVar);
+   return myRanVar;
+ }
 
-	    if(myRanVar==0)
+  public void calSalary()
+ {
+   empSalary=empWagePerHrs*empHrs;
+   System.out.println("Salary of the employee:" + empSalary);
+ }
+
+
+
+public static void main (String args[]){
+     System.out.println("---------------------------------------------------");
+     System.out.println("WELCOME TO EMPLOYEE WAGE PROBLEM");
+     System.out.println("---------------------------------------------------");
+     Attendance ewc=new Attendance();
+     int resultAttendance=ewc.empAttendance();
+     if(resultAttendance==0)
        {
-         System.out.println("Employee Absent");
+         System.out.println("Employee is Absent");
+         Attendance ewc1=new Attendance(20,0);
+         ewc1.calSalary();
        }
       else
        {
-         System.out.println("Employee Present");
+         System.out.println("Employee is Present");
+         Attendance ewc1=new Attendance(20,8);
+         ewc1.calSalary();
        }
  }
 
- public static void main (String args[]){
-     System.out.println("WELCOME TO EMPLOYEE WAGE PROBLEM");
-     Attendance ewc=new Attendance();
-     ewc.empAttendance();
- }
-
 }
+
